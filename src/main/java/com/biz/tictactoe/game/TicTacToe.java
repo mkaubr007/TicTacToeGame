9883f8f756d38e860250e500963f104872435944
mkaubr007 <mkaubr007@gmail.com>
2021-08-31 21:50:30 +0530

    dispBoard added

--- src/main/java/com/biz/tictactoe/game/TicTacToe.java
+++ src/main/java/com/biz/tictactoe/game/TicTacToe.java
@@ -2,7 +2,7 @@ package com.biz.tictactoe.game;
 
 public class TicTacToe {
     static int[][] BOARD = new int[3][3];
-
+    static boolean isEmpty=true;
     static void initBoard() {
         System.out.println("TIC TAC TOE GAME\nComputer is o\nPlayer  is x ");
         for (int i = 0; i < BOARD.length; i++) {
@@ -11,6 +11,29 @@ public class TicTacToe {
             }
         }
         System.out.println("Board is this :");
+        dispBoard();
+    }
+    static void dispBoard() {
+        int count = 0;
+        for (int i = 0; i < BOARD.length; i++) {
+            System.out.println("---------------");
+            System.out.print("||");
+            for (int j = 0; j < BOARD[i].length; j++) {
+                if (BOARD[i][j] == 0) {
+                    count++;
+                    System.out.print(" o |");
+                } else if (BOARD[i][j] == 1) {
+                    count++;
+                    System.out.print(" x |");
+                } else
+                    System.out.print("   |");
+            }
+            System.out.println("|");
+        }
+        if (count == 9) {
+            isEmpty = false;
+        }
+        System.out.println("---------------");
     }
     public static void main(String[] args) {
         System.out.println("Welcome to tic tac toe game");
