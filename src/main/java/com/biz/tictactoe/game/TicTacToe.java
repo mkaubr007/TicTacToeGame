10f66e197b9e972526f271c8e6aeffdee8e65877
mkaubr007 <mkaubr007@gmail.com>
2021-08-31 21:58:04 +0530

    putValue added

--- src/main/java/com/biz/tictactoe/game/TicTacToe.java
+++ src/main/java/com/biz/tictactoe/game/TicTacToe.java
@@ -1,6 +1,9 @@
 package com.biz.tictactoe.game;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+    static int player = 0;
     static int[][] BOARD = new int[3][3];
     static boolean isEmpty=true;
     static void initBoard() {
@@ -35,8 +38,32 @@ public class TicTacToe {
         }
         System.out.println("---------------");
     }
+    static void putVal() {
+        int i;
+        int j;
+        if (player % 2 == 1) {
+            i = (int) (Math.random() * 10) % 3;
+            j = (int) (Math.random() * 10) % 3;
+        } else {
+            Scanner s = new Scanner(System.in);
+            System.out.println("enter value of x and y by space");
+            i = s.nextInt();
+            j = s.nextInt();
+        }
+        if (BOARD[i][j] == -10) {
+            if (player % 2 == 0) {
+                BOARD[i][j] = 0;
+            } else {
+                BOARD[i][j] = 1;
+                System.out.println("Coumputer Choosing " + i + " " + j);
+            }
+        } else
+            putVal();
+
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to tic tac toe game");
         initBoard();
+        putVal();
     }
 }
