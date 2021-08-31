6c3d58893a741baa79aed457d778452ed8317c1a
mkaubr007 <mkaubr007@gmail.com>
2021-08-31 21:43:06 +0530

    initBoard added

--- src/main/java/com/biz/tictactoe/game/TicTacToe.java
+++ src/main/java/com/biz/tictactoe/game/TicTacToe.java
@@ -1,7 +1,19 @@
 package com.biz.tictactoe.game;
 
 public class TicTacToe {
+    static int[][] BOARD = new int[3][3];
+
+    static void initBoard() {
+        System.out.println("TIC TAC TOE GAME\nComputer is o\nPlayer  is x ");
+        for (int i = 0; i < BOARD.length; i++) {
+            for (int j = 0; j < BOARD[i].length; j++) {
+                BOARD[i][j] = -10;
+            }
+        }
+        System.out.println("Board is this :");
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to tic tac toe game");
+        initBoard();
     }
 }
